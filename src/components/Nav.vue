e2b64a010c10aa5b6fed406c5d6f2cae36bb07e4
apena124 <73714064+apena124@users.noreply.github.com>
2022-03-07 15:32:14 -0600

    advice

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,8 @@
 <template>
-    <div class="bg-yellow-500 text-green-700">
-    <div class="container mx-auto flex items-center justify-between">
-        <h1 class="tracking-tighter text-3xl font-thin">Basic<span class="font-normal">Router</span></h1>
-        <nav>
-            <ul class="flex space-x-4">
-                <router-link to="/"><li class="py-8 px-4 hover:cursor-pointer hover:bg-gray-400 hover:text-red-700">Home</li> </router-link>
-                <router-link to="/about"><li class="py-8 px-4 hover:cursor-pointer hover:bg-gray-400 hover:text-red-700">About</li></router-link>
-                <router-link to="/hello"><li class="py-8 px-4 hover:cursor-pointer hover:bg-gray-400 hover:text-red-700">Hello</li> </router-link>
-            </ul>
-        </nav>
+    <div class="text-green-700 bg-yellow-500">
+    <div class="container flex items-center justify-center mx-auto">
+        <h1 class="py-8 text-3xl font-thin tracking-tighter">Advice<span class="font-normal">Call</span></h1>
+        
     </div>
     </div>
 </template>
